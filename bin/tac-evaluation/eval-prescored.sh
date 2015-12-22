0c9fe11efdf65593ee6f20e27297b157c41fca5b
patverga <pat@cs.umass.edu>
2015-12-22 16:33:11 -0500

    fix script path

--- bin/tac-evaluation/eval-prescored.sh
+++ bin/tac-evaluation/eval-prescored.sh
@@ -7,5 +7,5 @@ TUNED_PARAMS=$3
 TAC_EVAL_ROOT=${TH_RELEX_ROOT}/bin/tac-evaluation
 source ${TAC_EVAL_ROOT}/configs/${YEAR}
 
-${TH_RELEX_ROOT}/eval-scripts/evaluateScoresTunedThresh $SCORE_SCRIPT $SCORED_CANDIDATES $RUN_DIR $CONFIG $PP $KEY $TUNED_PARAMS
+${TAC_EVAL_ROOT}/eval-scripts/evaluateScoresTunedThresh $SCORE_SCRIPT $SCORED_CANDIDATES $RUN_DIR $CONFIG $PP $KEY $TUNED_PARAMS
 
