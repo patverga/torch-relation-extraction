6918676f2f92e41353101c3dc89dc989ff42816e
patverga <pat@cs.umass.edu>
2015-12-22 19:52:23 -0500

    concise score candidate file

--- bin/tac-evaluation/score-tuned-concise.sh
+++ bin/tac-evaluation/score-tuned-concise.sh
@@ -15,16 +15,16 @@ source ${TAC_EVAL_ROOT}/configs/${YEAR}
 # score candidate file
 SCORED_CANDIDATES=`mktemp`
 CAND_SCORE_CMD="th ${TH_RELEX_ROOT}/src/eval/ScoreCandidateFile.lua -candidates $CANDIDATES -vocabFile $VOCAB -model $MODEL -gpuid $GPU -threshold 0 -outFile $SCORED_CANDIDATES -maxSeq $MAX_SEQ $EVAL_ARGS"
-echo ${CAND_SCORE_CMD}
+echo "Scoring candidate file: ${CAND_SCORE_CMD}"
 ${CAND_SCORE_CMD}
 
 # convert scored candidate to response file
 RESPONSE=`mktemp`
 RESPONSE_CMD=`${TAC_ROOT}/components/bin/response.sh ${RUN_DIR}/query_expanded.xml ${SCORED_CANDIDATES} ${RESPONSE}`
-echo ${RESPONSE_CMD}
+echo "Converting scored candidate to response file : ${RESPONSE_CMD}"
 ${RESPONSE_CMD}
 
 # post process and score response
 RES_SCORE_CMD=`${TAC_EVAL_ROOT}/score-responses.sh ${YEAR} ${RESPONSE}`
-echo ${RES_SCORE_CMD}
+echo "Evaluating response file : ${RES_SCORE_CMD}"
 ${RES_SCORE_CMD}
