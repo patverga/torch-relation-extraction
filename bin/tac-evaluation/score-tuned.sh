4cb1fdfdf796e306df794b0433976497c684db81
pv <pat@cs.umass.edu>
2015-12-29 11:19:12 -0500

    up script names

--- bin/tac-evaluation/score-tuned.sh
+++ bin/tac-evaluation/score-tuned.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 YEAR=$1
 MODEL=$2
@@ -9,19 +9,30 @@ TUNED_PARAMS=$6
 OUT=$7
 EVAL_ARGS=${@:8}
 
-
 TAC_EVAL_ROOT=${TH_RELEX_ROOT}/bin/tac-evaluation
 source ${TAC_EVAL_ROOT}/configs/${YEAR}
 
-SCORED_CANDIDATES=`mktemp`
-
 # score candidate file
-SCORE_CMD="th ${TH_RELEX_ROOT}/src/eval/ScoreCandidateFile.lua -candidates $CANDIDATES -vocabFile $VOCAB -model $MODEL -gpuid $GPU -threshold 0 -outFile $SCORED_CANDIDATES -maxSeq $MAX_SEQ $EVAL_ARGS"
-echo ${SCORE_CMD}
-${SCORE_CMD}
+SCORED_CANDIDATES=`mktemp`
+CAND_SCORE_CMD="th ${TH_RELEX_ROOT}/src/eval/ScoreCandidateFile.lua -candidates $CANDIDATES -vocabFile $VOCAB -model $MODEL -gpuid $GPU -threshold 0 -outFile $SCORED_CANDIDATES -maxSeq $MAX_SEQ $EVAL_ARGS"
+echo "Scoring candidate file: ${CAND_SCORE_CMD}"
+${CAND_SCORE_CMD}
 
-EVAL_CMD="${TAC_EVAL_ROOT}/eval-scripts/evaluateScoresTunedThresh $SCORE_SCRIPT $SCORED_CANDIDATES $RUN_DIR $CONFIG $PP $KEY $TUNED_PARAMS $OUT"
-echo ${EVAL_CMD}
-${EVAL_CMD}
+# threshold candidate file using tuned params
+THRESHOLD_CANDIDATE=`mktemp`
+echo "Thresholding candidate file :"
+${TAC_EVAL_ROOT}/eval-scripts/threshold-scored-candidates.sh ${SCORED_CANDIDATES} ${TUNED_PARAMS} ${THRESHOLD_CANDIDATE}
 
-rm ${SCORED_CANDIDATES}
+# convert scored candidate to response file
+echo "Converting scored candidate to response file"
+RESPONSE=`mktemp`
+${TAC_ROOT}/components/bin/response.sh ${RUN_DIR}/query_expanded.xml ${THRESHOLD_CANDIDATE} ${RESPONSE}
+
+echo "Post processing response for year $YEAR"
+RESPONSE_PP=`mktemp`
+${TAC_EVAL_ROOT}/post-process-response.sh $YEAR $PP $RUN_DIR $RESPONSE $RESPONSE_PP
+
+echo "Evaluating response"
+echo "`$SCORE_SCRIPT $RESPONSE_PP $KEY | grep -e F1 -e Recall -e Precision | tr '\n' '\t'`"
+
+cp $RESPONSE $OUT
