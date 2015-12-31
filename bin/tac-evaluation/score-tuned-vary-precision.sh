99e583bbc6bf4b9dfcea02ff0645bc05515f3c04
pv <pat@cs.umass.edu>
2015-12-31 10:41:38 -0500

    fix another script

--- bin/tac-evaluation/score-tuned-vary-precision.sh
+++ bin/tac-evaluation/score-tuned-vary-precision.sh
@@ -13,6 +13,8 @@ PARAM_P_BIAS="-0.5 -0.45 -0.4 -0.35 -0.3 -0.25 -0.2 -0.15 -0.1 -.05 0.0 0.05 0.1
 TAC_EVAL_ROOT=${TH_RELEX_ROOT}/bin/tac-evaluation
 source ${TAC_EVAL_ROOT}/configs/${YEAR}
 
+QUERY_EXPANDED=${RUN_DIR}/query_expanded.xml
+
 # score candidate file
 SCORED_CANDIDATES=`mktemp`
 CAND_SCORE_CMD="th ${TH_RELEX_ROOT}/src/eval/ScoreCandidateFile.lua -candidates $CANDIDATES -vocabFile $VOCAB -model $MODEL -gpuid $GPU -threshold 0 -outFile $SCORED_CANDIDATES -maxSeq $MAX_SEQ $EVAL_ARGS"
@@ -34,11 +36,11 @@ for bias in ${PARAM_P_BIAS}; do
     # convert scored candidate to response file
     echo "Converting scored candidate to response file"
     RESPONSE=`mktemp`
-    ${TAC_ROOT}/components/bin/response.sh ${RUN_DIR}/query_expanded.xml ${THRESHOLD_CANDIDATE} ${RESPONSE}
+    ${TAC_ROOT}/components/bin/response.sh $QUERY_EXPANDED ${THRESHOLD_CANDIDATE} ${RESPONSE}
 
     echo "Post processing response for year $YEAR"
     RESPONSE_PP=`mktemp`
-    ${TAC_EVAL_ROOT}/post-process-response.sh ${YEAR} ${PP} ${RUN_DIR} ${RESPONSE} ${RESPONSE_PP}
+    ${TAC_EVAL_ROOT}/post-process-response.sh ${YEAR} ${PP} ${QUERY_EXPANDED} ${RESPONSE} ${RESPONSE_PP}
 
     echo "Evaluating response"
     echo "`${SCORE_SCRIPT} ${RESPONSE_PP} ${KEY} | grep -e F1 -e Recall -e Precision | tr '\n' '\t'`"
