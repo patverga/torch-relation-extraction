6680b99dbcb93ffde46464844325104eaf514625
patverga <pat@cs.umass.edu>
2016-01-15 15:10:41 -0500

    remove .config files

--- setup-tac-eval.sh
+++ setup-tac-eval.sh
@@ -7,7 +7,7 @@ tar -xvzf candidates.tar.gz
 # make the various scoring scripts
 for y in 2 3 4; do
     cd ${TH_RELEX_ROOT}/bin/tac-evaluation/eval-scripts/SFScore-201${y}
-    javac  SFScore.java
+    javac SFScore.java
 done
 
 cd ${TH_RELEX_ROOT}
