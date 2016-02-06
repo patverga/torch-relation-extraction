efa91ac8bf2a45b6bc70fa9641f108c379772b99
pv <pat@cs.umass.edu>
2016-02-06 15:14:55 -0500

    tac eval with relation pooling

---
+++ src/eval/PoolClassifier.lua
@@ -0,0 +1,86 @@
+--
+-- User: pat
+-- Date: 2/5/16
+--
+
+package.path = package.path .. ";src/?.lua;src/nn-modules/?.lua;src/eval/?.lua"
+require 'SentenceClassifier'
+
+local PoolClassifier, parent = torch.class('PoolClassifier', 'SentenceClassifier')
+
+
+function PoolClassifier:__init(params)
+    parent:__init(params)
+    self.ep_pattern_map = {}
+    self.pattern_tensor_map = {}
+    self.ep_count = 0
+end
+
+
+--- process the candidate file and convert to torch ---
+function PoolClassifier:process_file(vocab_map, dictionary)
+    local line_num = 0
+    local max_seq = 0
+    local data = {}
+    print('Processing data')
+    for line in io.lines(self.params.candidates)
+    do
+        local enitity_pair, pattern, out_line, pattern_tensor, tac_tensor, seq_len = self:process_line(line, vocab_map, dictionary)
+        if pattern_tensor:size(2) <= self.params.maxSeq then
+            pattern_tensor = pattern_tensor:view(pattern_tensor:size(1), 1, pattern_tensor:size(2))
+            -- pad all patterns up to consistent max seq length
+            if pattern_tensor:size(3) < self.params.maxSeq then
+                local pad_length = self.params.maxSeq - pattern_tensor:size(3)
+                local pad_tensor = torch.Tensor(1, 1, pad_length):fill(self.params.padIdx)
+                pattern_tensor = pattern_tensor:cat(pad_tensor)
+            end
+            --TODO actually want this to use just the lstm part not the pool -- does this matter?
+            tac_tensor = tac_tensor:view(tac_tensor:size(1), 1, tac_tensor:size(2))
+
+            if not self.ep_pattern_map[enitity_pair] then self.ep_count = self.ep_count+1; self.ep_pattern_map[enitity_pair] = {} end
+            if not self.ep_pattern_map[enitity_pair][pattern] then self.ep_pattern_map[enitity_pair][pattern] = true end
+            if not self.pattern_tensor_map[pattern] then self.pattern_tensor_map[pattern] = pattern_tensor end
+
+            max_seq = math.max(seq_len, max_seq)
+            if not data[enitity_pair] then data[enitity_pair] = {out_line={}, tac_tensor={}} end
+            local enitity_pair_data = data[enitity_pair]
+            table.insert(enitity_pair_data.out_line, out_line)
+            table.insert(enitity_pair_data.tac_tensor, tac_tensor)
+
+            line_num = line_num + 1
+            if line_num % 10000 == 0 then io.write('\rline : ' .. line_num); io.flush() end
+        end
+    end
+    print ('\rProcessed ' .. line_num .. ' lines')
+    return data, max_seq
+end
+
+
+--- score the data returned by process_file ---
+function PoolClassifier:score_data(data, max_seq)
+    print('Scoring data')
+    -- open output file to write scored candidates file
+    local out_file = io.open(self.params.outFile, "w")
+
+    local ep_num = 0
+    for ep, patterns in pairs(self.ep_pattern_map) do
+        if ep_num % 10 == 0 then io.write('\rProcessing ep number ' .. ep_num .. ' of ' .. self.ep_count); io.flush() end
+        local ep_data = data[ep]
+        local ep_tensors_table = {}
+        for pattern, _ in pairs(patterns) do table.insert(ep_tensors_table, self.pattern_tensor_map[pattern]) end
+
+        local all_tac_encoded = self.kb_encoder:forward(self:to_cuda(nn.JoinTable(1)(ep_data.tac_tensor))):clone()
+        local ep_tensor = nn.JoinTable(2)(ep_tensors_table)
+        local ep_encoded = self.text_encoder:forward(self:to_cuda(ep_tensor)):clone():expand(all_tac_encoded:size())
+
+        local out_lines = ep_data.out_line
+        local x = { all_tac_encoded, ep_encoded }
+        local scores = self:to_cuda(nn.CosineDistance())(x):double()
+        for i = 1, #out_lines do
+            local score = math.max(self.params.threshold, scores[i])
+            out_file:write(out_lines[i] .. score .. '\n')
+        end
+        ep_num = ep_num + 1
+    end
+    out_file:close()
+end
--- src/eval/ScoreCandidateFile.lua
+++ src/eval/ScoreCandidateFile.lua
@@ -3,34 +3,11 @@
 -- Date: 9/17/15
 --
 
-package.path = package.path .. ";src/?.lua;src/nn-modules/?.lua"
-
-require 'torch'
-require 'rnn'
-require 'ViewTable'
-require 'ReplicateAs'
-require 'SelectLast'
-require 'VariableLengthJoinTable'
-require 'VariableLengthConcatTable'
-require 'NoUpdateLookupTable'
-require 'NoUnReverseBiSequencer'
-require 'WordDropout'
-require 'EncoderPool'
-
--- autograd
-grad = require 'autograd'
-grad.optimize(true) -- global
-
-local auto_term_2 = function(input)
-local cols = input[1]
-    local row = input[2]
-    local row_matrix = torch.expand(row, cols:size())
-    return row_matrix
-end
+package.path = package.path .. ";src/?.lua;src/nn-modules/?.lua;src/eval/?.lua"
 
 --[[
-    Takes a tac candidate file, tab seperated vocab idx file, and a trained uschema encoder model
-    and exports a scored candidtate file to outfile
+Takes a tac candidate file, tab seperated vocab idx file, and a trained uschema encoder model
+and exports a scored candidtate file to outfile
 ]]--
 local cmd = torch.CmdLine()
 cmd:option('-candidates', '', 'input candidate file')
@@ -43,6 +20,7 @@ cmd:option('-delim', ' ', 'delimiter to split lines on')
 cmd:option('-threshold', 0, 'scores will be max(threshold, score)')
 cmd:option('-gpuid', -1, 'Which gpu to use, -1 for cpu (default)')
 cmd:option('-unkIdx', 1, 'Index to map unknown tokens to')
+cmd:option('-padIdx', 2, 'Index to map unknown tokens to')
 cmd:option('-chars', false, 'Split tokens into characters')
 cmd:option('-relations', false, 'Use full relation vectors instead of tokens')
 cmd:option('-logRelations', false, 'Use log relation vectors instead of tokens')
@@ -51,238 +29,16 @@ cmd:option('-appendEs', false, 'append @es to end of relation')
 cmd:option('-normalizeDigits', true, 'map all digits to #')
 cmd:option('-tokenAppend', '', 'append this to the end of each token')
 cmd:option('-fullPath', false, 'use the full input pattern without any segmenting')
-
+cmd:option('-pool', false, 'pool all relations containing each entity pair to make decicions')
 
 local params = cmd:parse(arg)
-local function to_cuda(x) return params.gpuid >= 0 and x:cuda() or x end
-if params.gpuid >= 0 then require 'cunn'; cutorch.manualSeed(0); cutorch.setDevice(params.gpuid + 1) else require 'nn' end
-
-local in_vocab = 0
-local out_vocab = 0
-
-
---- convert sentence to tac tensor using tokens ---
-local function token_tensor(arg1_first, pattern_rel, vocab_map, dictionary, start_idx, end_idx, use_full_pattern)
-    local idx = 0
-    local token_ids = {}
-    local tokens = {}
-
-    local first_arg = arg1_first and '$ARG1' or '$ARG2'
-    local second_arg = arg1_first and '$ARG2' or '$ARG1'
-    if params.tokenAppend ~= '' then
-        first_arg = first_arg .. params.tokenAppend
-        second_arg = second_arg .. params.tokenAppend
-    end
-
-    for token in string.gmatch(pattern_rel, "[^" .. params.delim .. "]+") do
-        if dictionary[token] then token = dictionary[token]
-        elseif params.tokenAppend ~= '' then token = token .. params.tokenAppend
-        end
-        if (idx >= start_idx and idx < end_idx) or use_full_pattern then
-            if params.chars then
-                for c in token:gmatch"." do table.insert(tokens, c) end
-                table.insert(tokens, ' ')
-            else
-                table.insert(tokens, token)
-            end
-        end
-        idx = idx + 1
-    end
-
-    if not use_full_pattern then
-        table.insert(token_ids, vocab_map[first_arg] or params.unkIdx)
-        if params.chars then table.insert(token_ids, vocab_map[' '] or params.unkIdx) end
-    end
-
-    for i = 1, #tokens do
-        local token
-        if not params.logRelations or #tokens <= 4 or i <= 2 or i > #tokens -2 then
-            token = tokens[i]
-        elseif i == 3 then
-            token = '[' .. math.floor(torch.log(#tokens - 4)/torch.log(2)) .. ']'
-        end
-        if token then
-            if params.doubleVocab then token = token .. '_' .. (arg1_first and '$ARG1' or '$ARG2') end
-            local id = vocab_map[token] or params.unkIdx
-            table.insert(token_ids, id)
-            if id == params.unkIdx then out_vocab = out_vocab + 1 else in_vocab = in_vocab + 1 end
-        end
-    end
-
-    if not use_full_pattern then table.insert(token_ids, vocab_map[second_arg] or params.unkIdx) end
-    local pattern_tensor = torch.Tensor(token_ids)
-    return pattern_tensor, #tokens
-end
-
---- convert sentence to tac tensor using whole relation tensor ---
-local function rel_tensor(arg1_first, pattern_rel, vocab_map, start_idx, end_idx, use_full_pattern)
-    local rel_string
-    if not use_full_pattern then
-        local idx = 0
-        local tokens = {}
-        for token in string.gmatch(pattern_rel, "[^" .. params.delim .. "]+") do
-            if params.tokenAppend ~= '' then token = token .. params.tokenAppend end
-            if idx >= start_idx and idx < end_idx then table.insert(tokens, token) end
-            idx = idx + 1
-        end
-
-        local first_arg = arg1_first and '$ARG1' or '$ARG2'
-        if params.tokenAppend ~= '' then first_arg = first_arg .. params.tokenAppend end
-        rel_string = first_arg .. ' '
-        for i = 1, #tokens do
-            if not params.logRelations or #tokens <= 4 or i <= 2 or i > #tokens - 2 then
-                rel_string = rel_string .. tokens[i] .. ' '
-            elseif i == 3 then
-                rel_string = rel_string .. '[' .. math.floor(torch.log(#tokens - 4) / torch.log(2)) .. ']' .. ' '
-            end
-        end
-        local second_arg = arg1_first and '$ARG2' or '$ARG1'
-        if params.tokenAppend ~= '' then second_arg = second_arg .. params.tokenAppend end
-        rel_string = rel_string .. second_arg
-    else
-        rel_string = pattern_rel
-    end
-    if params.appendEs then rel_string = rel_string .. "@es" end
-    local id = -1
-    local len = 0
-    if vocab_map[rel_string] then
-        id = vocab_map[rel_string]
-        len = 1
-        in_vocab = in_vocab + 1
-    else
-        out_vocab = out_vocab + 1
-    end
-    local pattern_tensor = torch.Tensor({id})
-    return pattern_tensor, len
-end
-
---- process a single line from a candidate file ---
-local function process_line(line, vocab_map, dictionary)
-    local query_id, tac_rel, sf_2, doc_info, start_1, end_1, start_2, end_2, pattern_rel
-    = string.match(line, "([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)")
-
-    if params.normalizeDigits and not params.fullPath then pattern_rel = pattern_rel:gsub("%d", "") end
-
-    local tac_tensor = torch.Tensor({vocab_map[tac_rel] or params.unkIdx})
-
-    -- we only want tokens between the two args
-    local start_idx = tonumber(end_1)
-    local end_idx = tonumber(start_2)
-    local arg1_first = true
-    if (start_idx > end_idx) then
-        start_idx, end_idx, arg1_first = tonumber(end_2), tonumber(start_1), false
-    end
-
-    local pattern_tensor, seq_len
-    if params.relations then
-        pattern_tensor, seq_len = rel_tensor(arg1_first, pattern_rel, vocab_map, start_idx, end_idx, params.fullPath)
-    else
-        pattern_tensor, seq_len = token_tensor(arg1_first, pattern_rel, vocab_map, dictionary, start_idx, end_idx, params.fullPath)
-    end
-
-    pattern_tensor = pattern_tensor:view(1, pattern_tensor:size(1))
-    tac_tensor = tac_tensor:view(1, tac_tensor:size(1))
-    local out_line = query_id .. '\t' .. tac_rel .. '\t' .. sf_2 .. '\t' .. doc_info .. '\t'
-            .. start_1 .. '\t' .. end_1 .. '\t' .. start_2 .. '\t' .. end_2 .. '\t'
-
-    return out_line, pattern_tensor, tac_tensor, seq_len
-end
-
---- process the candidate file and convert to torch ---
-local function process_file(vocab_map, dictionary)
-    local line_num = 0
-    local max_seq = 0
-    local data = {}
-    print('Processing data')
-    for line in io.lines(params.candidates) do
-        local out_line, pattern_tensor, tac_tensor, seq_len = process_line(line, vocab_map, dictionary)
-        max_seq = math.max(seq_len, max_seq)
-        if not data[seq_len] then data[seq_len] = {out_line={}, pattern_tensor={}, tac_tensor={}} end
-        local seq_len_data = data[seq_len]
-        table.insert(seq_len_data.out_line, out_line)
-        table.insert(seq_len_data.pattern_tensor, pattern_tensor)
-        table.insert(seq_len_data.tac_tensor, tac_tensor)
-        line_num = line_num + 1
-        if line_num % 10000 == 0 then io.write('\rline : ' .. line_num); io.flush() end
-    end
-    print ('\rProcessed ' .. line_num .. ' lines')
-    return data, max_seq
-end
-
--- TODO this only works for uschema right now
-local function score_tac_relation(text_encoder, kb_encoder, pattern_tensor, tac_tensor)
-    if #text_encoder:findModules('nn.EncoderPool') > 0 then pattern_tensor = pattern_tensor:view(pattern_tensor:size(1), 1, pattern_tensor:size(2)) end
-    if #kb_encoder:findModules('nn.EncoderPool') > 0 then tac_tensor = tac_tensor:view(tac_tensor:size(1), 1, tac_tensor:size(2)) end
-
-    local tac_encoded = kb_encoder:forward(to_cuda(tac_tensor)):clone()
-    local pattern_encoded = text_encoder:forward(to_cuda(pattern_tensor)):clone()
-
-    if tac_encoded:dim() == 3 then tac_encoded = tac_encoded:view(tac_encoded:size(1), tac_encoded:size(3)) end
-    if pattern_encoded:dim() == 3 then pattern_encoded = pattern_encoded:view(pattern_encoded:size(1), pattern_encoded:size(3)) end
-    local x = { tac_encoded, pattern_encoded }
-
-    local score = to_cuda(nn.CosineDistance())(x):double()
-    --    local score = to_cuda(nn.Sum(2))(to_cuda(nn.CMulTable())(x)):double()
-    return score
-end
-
---- score the data returned by process_file ---
-local function score_data(data, max_seq, text_encoder, kb_encoder)
-    print('Scoring data')
-    -- open output file to write scored candidates file
-    local out_file = io.open(params.outFile, "w")
-    for seq_len = 1, math.min(max_seq, params.maxSeq) do
-        if data[seq_len] then
-            io.write('\rseq length : ' .. seq_len); io.flush()
-            local seq_len_data = data[seq_len]
-            --- batch
---            local start = 1
---            while start <= #seq_len_data do
-            local pattern_tensor = nn.JoinTable(1)(seq_len_data.pattern_tensor)
-            local tac_tensor = nn.JoinTable(1)(seq_len_data.tac_tensor)
-            local scores = score_tac_relation(text_encoder, kb_encoder, pattern_tensor, tac_tensor)
-            local out_lines = seq_len_data.out_line
-            for i = 1, #out_lines do
-                local score = math.max(params.threshold, scores[i])
-                out_file:write(out_lines[i] .. score .. '\n')
-            end
-        end
-    end
-    out_file:close()
-end
-
-local function load_maps()
-    local vocab_map = {}
-    for line in io.lines(params.vocabFile) do
-        local token, id = string.match(line, "([^\t]+)\t([^\t]+)")
-        if token and id then
-            id = tonumber(id)
-            if id > 1 then vocab_map[token] = id end
-        end
-    end
-    local dictionary = {}
-    if params.dictionary ~= '' then
-        for line in io.lines(params.dictionary) do
-            -- space seperated
-            local en, es = string.match(line, "([^\t]+) ([^\t]+)")
-            dictionary[es] = en
-        end
-    end
-    return vocab_map, dictionary
-end
-
 
 ---- main
-
+require 'SentenceClassifier'
+require 'PoolClassifier'
+local scorer = params.pool and PoolClassifier(params) or SentenceClassifier(params)
 -- process the candidate file
-local data, max_seq = process_file(load_maps())
-
--- load model
-local model = torch.load(params.model)
-local kb_encoder = to_cuda(model.kb_col_table and model.kb_col_table or (model.col_encoder and model.col_encoder or model.encoder))
-local text_encoder = to_cuda(model.text_encoder and model.text_encoder or (model.col_encoder and model.col_encoder or model.encoder))
-kb_encoder:evaluate();text_encoder:evaluate()
-
+local data, max_seq = scorer:process_file(scorer:load_maps())
 -- score and export candidate file
-score_data(data, max_seq, text_encoder, kb_encoder)
-print ('\nDone, found ' .. in_vocab .. ' in vocab tokens and ' .. out_vocab .. ' out of vocab tokens.')
+scorer:score_data(data, max_seq)
+print ('\nDone, found ' .. scorer.in_vocab .. ' in vocab tokens and ' .. scorer.out_vocab .. ' out of vocab tokens.')
