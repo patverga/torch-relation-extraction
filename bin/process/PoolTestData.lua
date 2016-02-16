6d83b0e5748348962b8b4a248b4a1de202d723b1
pv <pat@cs.umass.edu>
2016-02-15 19:00:12 -0500

    fix pool test data scropt

--- bin/process/PoolTestData.lua
+++ bin/process/PoolTestData.lua
@@ -16,46 +16,54 @@ local params = cmd:parse(arg)
 print('Loading ep-rel map from ' .. params.keyFile)
 local key = torch.load(params.keyFile)
 local ep_rel_map = {}
+local ep_seq_map = {}
 for a, dat in pairs(key) do
     if dat and torch.type(dat) == 'table' and dat.ep then
         for i = 1, dat.ep:size(1) do
-            ep_rel_map[dat.ep[i]] = dat.seq:narrow(1,i,1)
+            ep_rel_map[dat.ep[i]] = dat.rel:narrow(1,i,1)
+            ep_seq_map[dat.ep[i]] = dat.seq:narrow(1,i,1)
         end
     end
 end
 
 print('Converting data from ' .. params.inFile .. ' and exporting to ' .. params.outFile)
-local data = torch.load(params.inFile)[1]
+local data = torch.load(params.inFile)
 local mapped_data = {}
 local missing = 0
-for i = 1, data.ep:size(1) do
-    local ep = data.ep[i][1]
-    local rels = ep_rel_map[ep]
-    if rels then
-        local seq = rels:size(2)
-        local label = data.label[i]
-        local kb_rel = data.rel[i]
-        if not mapped_data[seq] then mapped_data[seq] = {col_seq={}, label ={}, row_seq ={}} end
---        table.insert(mapped_data[seq].ep, ep)
-        table.insert(mapped_data[seq].col_seq, rels)
-        table.insert(mapped_data[seq].label, label)
-        table.insert(mapped_data[seq].row_seq, kb_rel)
-    else
-        missing = missing + 1
+for _, sub_data in pairs(data) do
+    if torch.type(sub_data) == 'table' then
+        for i = 1, sub_data.row:size(1) do
+            local ep = sub_data.row[i][1]
+            local rels = ep_rel_map[ep]
+            local seq = ep_seq_map[ep]
+            if rels then
+                local k = rels:size(2)
+                local label = sub_data.label[i]
+                local kb_rel = sub_data.col[i]
+                local kb_rel_seq = sub_data.col_seq[i]
+                if not mapped_data[k] then mapped_data[k] = {row={}, row_seq ={}, col={}, col_seq={}, label ={}} end
+                table.insert(mapped_data[k].col, rels)
+                table.insert(mapped_data[k].col_seq, seq)
+                table.insert(mapped_data[k].label, label)
+                table.insert(mapped_data[k].row, kb_rel)
+                table.insert(mapped_data[k].row_seq, kb_rel_seq)
+            else
+                missing = missing + 1
+            end
+        end
     end
 end
 print('couldnt map ' .. missing .. ' entity pairs')
 
-for len, data in pairs(mapped_data) do
-    mapped_data[len].label = nn.JoinTable(1)(data.label):view(-1, 1)
-    mapped_data[len].col_seq = nn.JoinTable(1)(data.col_seq)
-    mapped_data[len].row_seq = nn.JoinTable(1)(data.row_seq):view(-1, 1)
-    mapped_data[len].row = mapped_data[len].row_seq
-    mapped_data[len].col = torch.Tensor(mapped_data[len].col_seq:size(1))
+for len, sub_data in pairs(mapped_data) do
+    mapped_data[len].label = nn.JoinTable(1)(sub_data.label)
+    mapped_data[len].col = nn.JoinTable(1)(sub_data.col)
+    mapped_data[len].col_seq = nn.JoinTable(1)(sub_data.col_seq)
+    mapped_data[len].row = nn.JoinTable(1)(sub_data.row)
+    -- todo, something seems wrong with row_seq
+    mapped_data[len].row_seq = nn.JoinTable(1)(sub_data.row_seq)
+    mapped_data[len].count = sub_data.label:size(1)
 end
-
-
-
 -- SAVE FILE
 torch.save(params.outFile, mapped_data)
 print('Done')
