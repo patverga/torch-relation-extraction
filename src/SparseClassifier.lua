4dfc8861d02511659e005fd45f2ab421520846e2
patverga <pat@cs.umass.edu>
2016-02-25 10:08:20 -0500

    up sparse lassifier

--- src/SparseClassifier.lua
+++ src/SparseClassifier.lua
@@ -65,18 +65,18 @@ end
 function SparseClassifier:build_network()
     local net = nn.Sequential()
         :add(nn.SparseLinear(self.train_data.num_cols, 41))
-        :add(nn.LogSoftMax(2, self.train_data.num_cols))
+        :add(nn.LogSoftMax())
     -- put the networks on cuda
     self:to_cuda(net)
     return net
 end
+
 
 function SparseClassifier:gen_subdata_batches_three_col(data, sub_data, batches, max_neg, shuffle)
     local start = 1
     local kb_indices = {}
     local kb_mapped = {}
     -- only keep kb exampels
-
     for i = 1, sub_data.row:size(1) do
         if kb_rels[sub_data.row[i]] then
            table.insert(kb_mapped,  kb_rels[sub_data.row[i]])
@@ -84,34 +84,32 @@ function SparseClassifier:gen_subdata_batches_three_col(data, sub_data, batches,
         end
         i = i + 1
     end
-    sub_data.row = torch.Tensor(kb_mapped)
-    local index = torch.LongTensor(kb_indices)
-    print({index, sub_data.col})
-    sub_data.col = sub_data.col:index(index)
-    local rand_order = shuffle and torch.randperm(sub_data.row:size(1)):long() or torch.range(1, sub_data.row:size(1)):long()
-    while start <= sub_data.row:size(1) do
-        local size = math.min(self.params.batchSize, sub_data.row:size(1) - start + 1)
-        local batch_indices = rand_order:narrow(1, start, size)
-        local row_batch = self.params.rowEncoder == 'lookup-table' and sub_data.row:index(1, batch_indices) or sub_data.row_seq:index(1, batch_indices)
-        local col_batch = self.params.colEncoder == 'lookup-table' and sub_data.col:index(1, batch_indices) or sub_data.col_seq:index(1, batch_indices)
-        table.insert(batches, { data = col_batch, label = row_batch })
-        start = start + size
+    if #kb_indices > 0 then
+        sub_data.row = torch.Tensor(kb_mapped)
+        local index = torch.LongTensor(kb_indices)
+        sub_data.col = sub_data.col:index(1, index)
+        local rand_order = shuffle and torch.randperm(sub_data.row:size(1)):long() or torch.range(1, sub_data.row:size(1)):long()
+        while start <= sub_data.row:size(1) do
+            local size = math.min(self.params.batchSize, sub_data.row:size(1) - start + 1)
+            local batch_indices = rand_order:narrow(1, start, size)
+            local row_batch = sub_data.row:index(1,batch_indices)
+            local col_batch = sub_data.col:index(1,batch_indices)
+            col_batch = col_batch:view(col_batch:size(2), 1)
+            col_batch = col_batch:cat(torch.ones(col_batch:size(1)))
+            table.insert(batches, { data = col_batch, label = row_batch })
+            start = start + size
+        end
     end
 end
 
+
 function SparseClassifier:score_subdata(sub_data)
     local batches = {}
-    if sub_data.ep then self:gen_subdata_batches_four_col(sub_data, sub_data, batches, 0, false)
-    else self:gen_subdata_batches_three_col(sub_data, sub_data, batches, 0, false) end
+    self:gen_subdata_batches_three_col(sub_data, sub_data, batches, 0, false)
 
     local scores = {}
     for i = 1, #batches do
-        local row_batch, col_batch, _ = unpack(batches[i].data)
-        if self.params.colEncoder == 'lookup-table' then col_batch = col_batch:view(col_batch:size(1), 1) end
-        if self.params.rowEncoder == 'lookup-table' then row_batch = row_batch:view(row_batch:size(1), 1) end
-        local encoded_rel = self.col_encoder(self:to_cuda(col_batch)):squeeze():clone()
-        local encoded_ep = self.row_encoder(self:to_cuda(row_batch))
-        local score = self:to_cuda(nn.PairwiseDistance(self.params.p))({encoded_ep[1] + encoded_rel, encoded_ep[2]})
+        local score = self.net(batches[i].data)[batches[i].label[1]]
         table.insert(scores, score)
     end
     return scores, sub_data.label:view(sub_data.label:size(1))
